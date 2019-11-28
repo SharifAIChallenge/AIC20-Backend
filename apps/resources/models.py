b8323a0d3d9bfd29dbbdcb7e020b34b548567cdb
alireza <alirezashateri7@gmail.com>
2019-11-28 15:48:27 +0330

    prerequistics

---
+++ apps/resources/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class Section(models.Model):
+    document = models.ForeignKey(Document, related_name='sections', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+
+class Subtitle(models.Model):
+    subtitle = models.CharField(max_length=50)
+    section = models.ForeignKey(Section, related_name='subtitles', on_delete=models.CASCADE)
---
+++ apps/resources/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+
+
+class Document(APIView):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+
+class Section(APIView):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
