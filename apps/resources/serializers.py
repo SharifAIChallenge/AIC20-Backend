5f43cf56535cd88768f2275c5d89f57bb4e9e365
alireza <alirezashateri7@gmail.com>
2019-11-28 17:11:49 +0330

    prerequistics :)

---
+++ apps/resources/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Document, Section, Subtitle
+
+
+class DocumentSerializer(ModelSerializer):
+    pass
+
+
+class SectionSerializer(ModelSerializer):
+    pass
+
+
+class SubtitleSerializer(ModelSerializer):
+    pass
