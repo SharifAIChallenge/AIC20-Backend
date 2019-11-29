806d0815b928e84141469767917831efbdb1c561
HKalbasi <kalbasi@ce.sharif.edu>
2019-11-29 23:29:06 +0330

    fix test

--- thebackend/tests.py
+++ thebackend/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
+# Remove this and add your test case here.
 
 class AnimalTestCase(TestCase):
     def test(self):
-        self.assertEqual(1, 2, 'hoora we faild')
+        self.assertEqual(1, 1, 'hoora we did not failed')
