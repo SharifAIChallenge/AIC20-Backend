7308d401a7c13ccbbf78d5fa31ebf8cc26e98ff1
HKalbasi <kalbasi@ce.sharif.edu>
2019-11-29 23:31:39 +0330

    fix linting error

--- thebackend/tests.py
+++ thebackend/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 
 # Remove this and add your test case here.
+# For learn how to add test case, visit:
+# https://docs.djangoproject.com/en/2.2/topics/testing/overview/
+
 
 class AnimalTestCase(TestCase):
     def test(self):
