2ddded4db1e649f38b5579c7d64527125a5288e6
salehos <salehos763@gmail.com>
2019-12-06 12:20:31 +0330

    Fix admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.db import models
+from martor.widgets import AdminMartorWidget
 
-# Register your models here.
+from blog.models import Post, Comment, Tag
+
+
+@admin.register(Post)
+class PostAdmin(ModelAdmin):
+
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMartorWidget},
+    }
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(ModelAdmin):
+    comment_shown_editable = ['shown']
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMartorWidget},
+    }
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMartorWidget},
+    }
+    pass
