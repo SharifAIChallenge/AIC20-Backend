dddd39d12894c7cf441b5c8dcddaa696b16eddc9
HKalbasi <kalbasi@ce.sharif.edu>
2019-12-14 17:42:40 +0330

    add admin widget

--- apps/translation/admin.py
+++ apps/translation/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
 from .models import TranslatedText, DocumentTest
+from django.contrib.admin import widgets
+from martor.widgets import AdminMartorWidget
+from django.db import models
+from django_reverse_admin import ReverseModelAdmin, ReverseInlineModelAdmin
 
+class TranslatedTextInlineSmall(ReverseInlineModelAdmin):
+    model = TranslatedText
+    formfield_overrides = {
+        models.TextField: {'widget': widgets.AdminTextInputWidget},
+    }
 
 @admin.register(TranslatedText)
 class TranslatedTextAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMartorWidget},
+    }
 
 
 @admin.register(DocumentTest)
-class DocumentTestAdmin(admin.ModelAdmin):
-    pass
+class DocumentTestAdmin(ReverseModelAdmin):
+    inline_type = 'tabular'
+    inline_reverse = [
+        {
+            'field_name': 'title', 
+            'kwargs': {},
+            'admin_class': TranslatedTextInlineLarge
+        }
+    ]
