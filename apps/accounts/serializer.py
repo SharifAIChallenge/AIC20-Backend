baa77af1ba832cc35e96bd082acdb81b45ba37f2
rohamrahimi <45598680+rohamrahimi@users.noreply.github.com>
2019-12-14 10:08:37 +0330

    Update serializer.py

--- apps/accounts/serializer.py
+++ apps/accounts/serializer.py
@@ -22,10 +22,3 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create(**validated_data)
         Profile.objects.create(user=user, **profile_data)
         return user
-
-
-
-
-
-
-
