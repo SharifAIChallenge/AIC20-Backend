998af10ca97c054b75253ce881f0e1a70ad25f1a
mrtaalebi <the.doors.are.locked@gmail.com>
2019-12-16 12:19:06 +0330

    Split martor out of development settings

--- thebackend/settings/development.py
+++ thebackend/settings/development.py
@@ -137,71 +137,9 @@ REST_FRAMEWORK = {
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-    ]
-}
-
-STATIC_ROOT = './statics'
-
-MARTOR_ENABLE_CONFIGS = {
-    'imgur': 'true',  # to enable/disable imgur/custom uploader.
-    'mention': 'false',  # to enable/disable mention
-    'jquery': 'true',
-    # to include/revoke jquery (require for admin default django)
-    'living': 'false',  # to enable/disable live updates in preview
-    'spellcheck': 'true',
-}
-
-MARTOR_ENABLE_LABEL = False
-# Imgur API Keys
-MARTOR_IMGUR_CLIENT_ID = 'your-client-id'
-MARTOR_IMGUR_API_KEY = 'your-api-key'
-
-# Safe Mode
-MARTOR_MARKDOWN_SAFE_MODE = True  # default
-
-# Markdownify
-MARTOR_MARKDOWNIFY_FUNCTION = 'martor.utils.markdownify'  # default
-MARTOR_MARKDOWNIFY_URL = '/martor/markdownify/'  # default
-
-# Markdown extensions (default)
-MARTOR_MARKDOWN_EXTENSIONS = [
-    'markdown.extensions.extra',
-    'markdown.extensions.nl2br',
-    'markdown.extensions.smarty',
-    'markdown.extensions.fenced_code',
-
-    # Custom markdown extensions.
-    'martor.extensions.urlize',
-    'martor.extensions.del_ins',  # ~~strikethrough~~ and ++underscores++
-    'martor.extensions.mention',  # to parse markdown mention
-    'martor.extensions.emoji',  # to parse markdown emoji
-    'martor.extensions.mdx_video',  # to parse embed/iframe video
-]
-
-# Markdown Extensions Configs
-MARTOR_MARKDOWN_EXTENSION_CONFIGS = {}
-
-# Markdown urls
-MARTOR_UPLOAD_URL = '/martor/uploader/'  # default
-MARTOR_SEARCH_USERS_URL = '/martor/search-user/'  # default
-
-# Markdown Extensions
-MARTOR_MARKDOWN_BASE_EMOJI_URL = \
-    'https://github.githubassets.com/images/icons/emoji/'
-# default from github
-MARTOR_MARKDOWN_BASE_EMOJI_URL = \
-    'https://github.githubassets.com/images/icons/emoji/'
-# please change this to your domain
-MARTOR_MARKDOWN_BASE_MENTION_URL = 'https://python.web.id/author/'
-
-CSRF_COOKIE_HTTPONLY = False
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS':
-    'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
+    'PAGE_SIZE': 100,
 }
+
+from .martor import *
