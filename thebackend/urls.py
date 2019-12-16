8951cd1251cd875e75c455b534a69f24d29779d4
mrtaalebi <the.doors.are.locked@gmail.com>
2019-12-16 18:34:30 +0330

    Add media url

--- thebackend/urls.py
+++ thebackend/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('apps.homepage.urls')),
@@ -26,3 +29,8 @@ urlpatterns = [
     path('translation/', include('apps.translation.urls')),
     path('blog/', include('apps.blog.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
