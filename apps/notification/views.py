fd88a7c84a4ef3335a6b5bd796107313bb814e3b
mhbahmani <mhbahmani79@gmail.com>
2019-12-19 23:00:31 +0330

    NotificationView fixed

--- apps/notification/views.py
+++ apps/notification/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
@@ -10,10 +12,11 @@ from apps.notification.models import Notification
 from apps.notification.serializers import NotificationSerializer
 
 
+@permission_classes([IsAuthenticated])
 class NotificationView(GenericAPIView):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
 
     def get(self, request):
-        data = self.get_serializer(self.get_queryset().filter(request.user.is_authenticated)).data
+        data = self.get_serializer(self.get_queryset().filter(user=request.user)).data
         return Response(data, status=status.HTTP_200_OK)
