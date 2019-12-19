bc7a7e1cd9410adbbc3b22028e7fd7ad43ab678c
HKalbasi <kalbasi@ce.sharif.edu>
2019-12-20 00:19:48 +0330

    create reset password view

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -9,6 +9,9 @@ from rest_framework.status import HTTP_200_OK
 from apps.accounts.models import Profile
 from apps.accounts.serializer import *
 
+from django_rest_passwordreset.signals import reset_password_token_created
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 
 class SignUpView(GenericAPIView):
     queryset = Profile.objects.all()
@@ -48,3 +51,31 @@ class LoginWithGoogleView(GenericAPIView):
 
     def post(self, request):
         pass
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+
+    context = {
+        'current_user': reset_password_token.user,
+        'username': reset_password_token.user.username,
+        'email': reset_password_token.user.email,
+        'reset_password_url': "{}?token={}".format(reverse('password_reset:reset-password-request'),
+                                                   reset_password_token.key)
+    }
+
+    # render email text
+    email_html_message = render_to_string('email/user_reset_password.html', context)
+    email_plaintext_message = render_to_string('email/user_reset_password.txt', context)
+
+    msg = EmailMultiAlternatives(
+        # title:
+        "Password Reset for {title}".format(title="Some website title"),
+        # message:
+        email_plaintext_message,
+        # from:
+        "noreply@somehost.local",
+        # to:
+        [reset_password_token.user.email]
+    )
+    msg.attach_alternative(email_html_message, "text/html")
+    msg.send()
