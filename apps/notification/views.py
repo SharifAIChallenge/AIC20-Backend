bd9830f5edcb1b78c5ecbb4e1156e016ea7ab93a
mhbahmani <mhbahmani79@gmail.com>
2019-12-19 15:35:17 +0330

    NotificationView added

--- apps/notification/views.py
+++ apps/notification/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from requests import Response
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+
+from apps.notification.models import Notification
+from apps.notification.serializers import NotificationSerializer
+
+
+class NotificationView(GenericAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+    def get(self, request):
+        data = self.get_serializer(self.get_queryset().filter(request.user.is_authenticated)).data
+        return Response(data, status=status.HTTP_200_OK)
