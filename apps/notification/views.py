6741a08fba45b84a11e1b7fe8d4a9c6a43afe7d6
mhbahmani <mhbahmani79@gmail.com>
2019-12-19 17:37:35 +0330

    NotificationView response bug fixed

--- apps/notification/views.py
+++ apps/notification/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
-from requests import Response
+from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 
