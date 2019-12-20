072fa6de0ae502caa70464a43653b91d920af464
mhbahmani <mhbahmani79@gmail.com>
2019-12-20 19:51:46 +0330

    emailAdmin added

--- apps/notification/admin.py
+++ apps/notification/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 
-
 # Register your models here.
+from django.db import models
+from martor.widgets import AdminMartorWidget
+
 from apps.notification.models import Notification
 
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Notification)
+class EmailAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMartorWidget},
+    }
