4178fbffad632c07c721bff9405d6ec2444262b9
mrtaalebi <the.doors.are.locked@gmail.com>
2019-12-26 13:22:08 +0330

    Compress resources urls

--- apps/resources/urls.py
+++ apps/resources/urls.py
@@ -5,8 +5,8 @@ from .views import DocumentListAPIView, DocumentInstanceAPIView, SectionAPIView
 app_name = 'resource'
 
 urlpatterns = [
-    path('documents/', DocumentListAPIView.as_view(), name='documents_list'),
-    path('documents/<doc_id>/sections',
+    path('', DocumentListAPIView.as_view(), name='documents_list'),
+    path('<int:doc_id>',
          DocumentInstanceAPIView.as_view(), name='document_sections'),
     path('sections/<section_uuid>', SectionAPIView.as_view(), name='section'),
 ]
