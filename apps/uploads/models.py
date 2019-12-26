bedd8c1bd8b9e067459a2e2dd43f591e6dcefe13
mrtaalebi <the.doors.are.locked@gmail.com>
2019-12-26 14:03:08 +0330

    Fix fileUpload __str__

--- apps/uploads/models.py
+++ apps/uploads/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.conf import settings
 
 class FileUpload(models.Model):
     file = models.FileField(upload_to='admin_file_uploads')
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name + ':\t' + self.file.name
+        return self.name + ':\t' + settings.MEDIA_URL + self.file.name
