22570165a913f74b4748cd3d72d1292abaf1fc46
alireza <alirezashateri7@gmail.com>
2020-01-15 23:59:15 +0330

    Add Submit and MatchList views

--- apps/scoreboard/views.py
+++ apps/scoreboard/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+
 
 # Create your views here.
+
+class Submit(GenericAPIView):
+    def post(self, request):
+        pass
+
+
+class MatchList(GenericAPIView):
+    def get(self, request):
+        pass
