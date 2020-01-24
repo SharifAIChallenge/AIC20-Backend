72c35b4dfe4ba1852d4809f16fee960b72fd3a26
alireza <alirezashateri7@gmail.com>
2020-01-24 13:01:03 +0330

    Update requirements.txt

--- apps/participation/admin.py
+++ apps/participation/admin.py
@@ -3,13 +3,16 @@ from django.contrib.admin import ModelAdmin
 
 from .models import Badge, Participant, Team
 
+
 @admin.register(Badge)
 class BadgeAdmin(ModelAdmin):
     pass
 
+
 @admin.register(Participant)
 class ParticipantAdmin(ModelAdmin):
     pass
+
 
 @admin.register(Team)
 class TeamAdmin(ModelAdmin):
