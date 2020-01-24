05f259d2b1906b61e26a25dcf94b5e923a5a9852
Rez <reza.amini.majd1379@gmail.com>
2020-01-24 13:11:40 +0330

    remove score model

--- apps/scoreboard/models.py
+++ apps/scoreboard/models.py
@@ -7,11 +7,7 @@ from django.db import models
 class Row(models.Model):
     team = models.ForeignKey('participation.Participant', related_name='rows', on_delete=models.CASCADE)
     scoreboard = models.ForeignKey('scoreboard.ScoreBoard', related_name='rows', on_delete=models.CASCADE)
-
-
-class Score(models.Model):
-    row = models.OneToOneField('scoreboard.Row', related_name='score', on_delete=models.CASCADE)
-    number = models.FloatField(default=1000.0)
+    score = models.FloatField(default=1000.0)
 
 
 class ScoreBoard(models.Model):
