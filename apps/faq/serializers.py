c700e09fdc9cf365c5de061d79c5dea3e0aedc9d
alireza <alirezashateri7@gmail.com>
2020-01-30 09:30:45 +0330

    Add faq app serializers

--- apps/faq/serializers.py
+++ apps/faq/serializers.py
@@ -1,1 +1,15 @@
 from rest_framework.serializers import ModelSerializer
+
+
+class AnswerSerializer(ModelSerializer):
+    class Meta:
+        model = ModelSerializer
+        fields = ['id', 'title', 'body', 'question']
+
+
+class QuestionSerializer(ModelSerializer):
+    answer = AnswerSerializer(read_only=True)
+
+    class Meta:
+        model = ModelSerializer
+        fields = ['id', 'title', 'body', 'answer']
