53d0515962f552f5e04ec323d2bc942fb7e9d7cf
alireza <alirezashateri7@gmail.com>
2020-01-30 10:42:32 +0330

    Add staff app serializers

--- apps/staff/serializers.py
+++ apps/staff/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework.serializers import ModelSerializer
 
+from . import models as staff_models
+
+
+class StaffSerializer(ModelSerializer):
+    class Meta:
+        fields = ['__all__']
--- apps/staff/views.py
+++ apps/staff/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
