842f2c7129ca297a0401811cf6d4a02ddb3ad41c
alireza <alirezashateri7@gmail.com>
2020-01-30 09:40:51 +0330

    Add faq app view

--- apps/faq/views.py
+++ apps/faq/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+from . import serializers as faq_serializers
+from . import models as faq_models
+
 
 # Create your views here.
+
+class QuestionsListAPIView(GenericAPIView):
+    queryset = faq_models.Question.objects.all()
+    serializer_class = faq_serializers.QuestionSerializer
+
+    def get(self, request):
+        data = self.get_serializer(self.get_queryset()).data
+        return Response(data={'questions': data}, status=status.HTTP_200_OK)
