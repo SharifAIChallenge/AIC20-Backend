f5ae6cacd16ec733c3a05436cfc56c9dd2417ab4
alireza <alirezashateri7@gmail.com>
2020-02-03 03:25:20 +0330

    Fix staff app bug

--- apps/staff/urls.py
+++ apps/staff/urls.py
@@ -6,5 +6,4 @@ app_name = 'staff'
 
 urlpatterns = [
     path('staffs', views.StaffsListAPIView.as_view(), name='staffs_list'),
-    path('staffs/<str:title>', views.StaffsByTitleListAPIView.as_view(), name='staffs_by_title_list'),
 ]
