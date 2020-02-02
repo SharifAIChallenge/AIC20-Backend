3afdc567e2735faa33ec8e96ffb26e9097b17255
alireza <alirezashateri7@gmail.com>
2020-02-02 13:03:48 +0330

    Add admin models for scoreboard app models

--- apps/scoreboard/admin.py
+++ apps/scoreboard/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
+from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin
+
+from . import models as scoreboard_models
+
+
 # Register your models here.
+
+class RowInline(admin.StackedInline):
+    model = scoreboard_models.Row
+
+
+@admin.register(scoreboard_models.ScoreBoard)
+class ScoreBoardAdmin(PolymorphicParentModelAdmin):
+    inlines = [RowInline]
+    base_model = scoreboard_models.ScoreBoard
+
+    child_models = [scoreboard_models.ChallengeScoreBoard, scoreboard_models.GroupScoreBoard]
+
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(scoreboard_models.ChallengeScoreBoard)
+class ChallengeScoreBoardAdmin(PolymorphicChildModelAdmin):
+    inlines = [RowInline]
+    base_model = scoreboard_models.ScoreBoard
+    show_in_index = True
+
+
+@admin.register(scoreboard_models.GroupScoreBoard)
+class GroupScoreBoardAdmin(PolymorphicChildModelAdmin):
+    inlines = [RowInline]
+    base_model = scoreboard_models.ScoreBoard
+    show_in_index = True
