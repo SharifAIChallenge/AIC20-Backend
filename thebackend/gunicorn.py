d3a0b5528ce60837f393434f1f2a58f9d1a5243d
alireza <alirezashateri7@gmail.com>
2020-02-03 19:28:27 +0330

    Fix requirements.txt for celery version

--- thebackend/gunicorn.py
+++ thebackend/gunicorn.py
@@ -3,9 +3,10 @@ bind = '0.0.0.0:8000'
 workers = 4
 proc_name = 'aic gunicorn'
 
-preload=True
-timeout=60
+preload = True
+timeout = 60
 
 loglevel = 'debug'
 
 errorlog = '/var/log/thebackend/gunicorn_error.log'
+
