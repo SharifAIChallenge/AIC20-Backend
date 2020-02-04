ab60d0867f5eb212fb80c9d9206f809f9130cbd1
alireza <alirezashateri7@gmail.com>
2020-02-04 20:00:31 +0330

    Fix Go destination uniqunes bug

--- apps/go/models.py
+++ apps/go/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+
+
 # Create your models here.
 
 class Redirect(models.Model):
     source = models.CharField(max_length=50, unique=True)
-    destination = models.CharField(max_length=50, unique=True)
+    destination = models.CharField(max_length=50)
 
     def __str__(self):
         return '%s %s' % (self.source, self.destination)
