1d687e6a56e4fb66df4d965fd1bb116ab11c2997
alireza <alirezashateri7@gmail.com>
2020-02-09 21:44:56 +0330

    public notifications

--- apps/notification/urls.py
+++ apps/notification/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.notification.views import NotificationView, SubscriberView, PublicNotificationsAPIView
 
 urlpatterns = [
-    path('', NotificationView.as_view(), name='notifications_list')
+    path('', NotificationView.as_view(), name='notifications_list'),
     path('public-notifications', PublicNotificationsAPIView.as_view(), name='public_notifications_list'),
     path('email', SubscriberView.as_view(), name='subscribe'),
 
