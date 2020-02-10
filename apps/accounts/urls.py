da7efbab9c9d5ce313453bdeaeb4b99e6092ad28
alireza <alirezashateri7@gmail.com>
2020-02-10 18:10:37 +0330

    Add ResendActivationEmailAPIView

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,9 +2,12 @@ from django.urls import path, include
 from rest_framework.authtoken import views
 from apps.accounts.views import *
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('login', views.obtain_auth_token),
     path('signup', SignUpView.as_view()),
+    path('resend-activation-link', ResendActivationEmailAPIView.as_view()),
     path('activate/<slug:eid>/<slug:token>', ActivateView.as_view()),
     path('logout', LogoutView.as_view()),
     path('profile', ProfileView.as_view()),
