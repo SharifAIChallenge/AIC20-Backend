5ceb773eeaea5d79f28fdd507dc83a08ad9c0fd1
alireza <alirezashateri7@gmail.com>
2020-02-11 17:47:03 +0330

    Celery settings

--- thebackend/settings/celery.py
+++ thebackend/settings/celery.py
@@ -1,12 +1,22 @@
+from __future__ import absolute_import, unicode_literals
 import os
-from celery import Celery
+from logging.config import dictConfig
+
 from django.conf import settings
+from celery import Celery
+from celery.signals import setup_logging
 
 """ RabbitMQ as message broker
 """
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thebackend.settings.development')
-
 app = Celery(main='thebackend')
 app.config_from_object('django.conf:settings')
+
+
+@setup_logging.connect
+def config_loggers(*args, **kwargs):
+    dictConfig(settings.LOGGING)
+
+
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
