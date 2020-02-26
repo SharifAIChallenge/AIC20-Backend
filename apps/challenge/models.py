9fb3285ca830abb28a4f3fc3db2d99e05bd15523
alireza <alirezashateri7@gmail.com>
2020-02-26 23:53:49 +0330

    Add postfix to log file name

--- apps/challenge/models.py
+++ apps/challenge/models.py
@@ -249,8 +249,8 @@ class Submission(models.Model):
         self.save()
 
     def handle(self):
-        handle_submission(self.id)
-        # handle_submission.delay(self.id)
+        # handle_submission(self.id)
+        handle_submission.delay(self.id)
 
     def upload(self):
         from . import functions
--- apps/challenge/views.py
+++ apps/challenge/views.py
@@ -276,7 +276,7 @@ def report(request):
             response = functions.download_file(single_report['parameters']['graphic_log'])
             test += "  logfile ro download kard"
             game.status = 'done'
-            game.log = ContentFile(name=single_report['parameters']['graphic_log'], content=response.text)
+            game.log = ContentFile(name=single_report['parameters']['graphic_log'] + ".json", content=response.text)
             game.save()
             game.update_scores()
         elif single_report['status'] == 3:
