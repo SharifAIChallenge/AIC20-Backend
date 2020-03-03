464c50a364f86d6df8ee9322511923d2887e0cc0
alireza <alirezashateri7@gmail.com>
2020-03-03 04:08:35 +0330

    Friendly scoreboard

--- apps/scoreboard/views.py
+++ apps/scoreboard/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from .models import ChallengeScoreBoard, GroupScoreBoard, ScoreBoardTypes
+from .models import ChallengeScoreBoard, GroupScoreBoard, FriendlyScoreBoard, ScoreBoardTypes
 from ..challenge.models import Challenge, ChallengeTypes, Group
 from .serializers import RowSerializer
 
