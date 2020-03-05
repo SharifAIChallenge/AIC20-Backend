0cb42c3fd3454eae558de843f6e43ea0ee16e41d
alireza <alirezashateri7@gmail.com>
2020-03-05 12:23:47 +0330

    Modify Team games list api

--- apps/challenge/views.py
+++ apps/challenge/views.py
@@ -92,8 +92,7 @@ class GamesListAPIView(GenericAPIView):
     def get(self, request):
         if not hasattr(request.user, 'participant'):
             return Response(data={'errors': ['Sorry! you dont have a team']}, status=status.HTTP_406_NOT_ACCEPTABLE)
-        game_ids = challenge_models.GameTeam.objects.filter(team=request.user.participant.team).filter(
-            game_side__game__match=None).values_list(
+        game_ids = challenge_models.GameTeam.objects.filter(team=request.user.participant.team).values_list(
             'game_side__game_id', flat=True)
         data = self.get_serializer(self.get_queryset().filter(id__in=game_ids), many=True).data
         return Response(data={'games': data}, status=status.HTTP_200_OK)
