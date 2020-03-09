a6cc9970a525442a4ca45ebd60c36dc425ee72d9
alireza <alirezashateri7@gmail.com>
2020-03-09 17:35:36 +0330

    Modify scoreboard update for final challenge frendlies

--- apps/challenge/models.py
+++ apps/challenge/models.py
@@ -307,10 +307,11 @@ class Game(models.Model):
         from apps.challenge.services.utils import update_game_team_scoreboard_score
 
         friendly_scoreboard = ScoreBoard.objects.get(type=ScoreBoardTypes.FRIENDLY)
+        if game_teams[0].challenge.type == ChallengeTypes.FINAL:
+            friendly_scoreboard = game_teams[0].challenge.scoreboard
         update_game_team_scoreboard_score(game=self, scoreboard=friendly_scoreboard)
         for game_team in game_teams:
             row = friendly_scoreboard.rows.get(team=game_team.team)
-            # row.score += game_team.score
             row.wins, row.draws, row.loss = Stats(team=game_team.team, friendly_only=True)()
             row.save()
 
