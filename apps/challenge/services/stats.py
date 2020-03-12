de7446094989482f6fd268ef644b2d8aae72b612
alireza <alirezashateri7@gmail.com>
2020-03-12 17:53:59 +0330

    Dont show final matches

--- apps/challenge/services/stats.py
+++ apps/challenge/services/stats.py
@@ -21,33 +21,34 @@ class Stats:
 
     def _wins(self):
         from apps.challenge.models import GameTeam
+        challenge = Challenge.objects.get(type=ChallengeTypes.FINAL)
 
         game_teams = GameTeam.objects.filter(game_side__game__status=SingleGameStatusTypes.DONE).filter(
             team=self.team).filter(
-            game_side__has_won=True)
+            game_side__has_won=True).filter(game_side__game__time__lt=challenge.end_time)
         if self.friendly_only:
             game_teams = game_teams.filter(game_side__game__match=None)
         if self.tournament_only:
             game_teams = game_teams.exclude(game_side__game__match=None)
         if self.final_challenge:
-            time = Challenge.objects.get(type=ChallengeTypes.FINAL).start_time
-            game_teams = game_teams.filter(game_side__game__time__gt=time)
+            game_teams = game_teams.filter(game_side__game__time__gt=challenge.start_time)
             self.wins = game_teams.count() // 2
         else:
             self.wins = game_teams.count()
 
     def _loss_and_draws(self):
         from apps.challenge.models import GameTeam, Game
+        challenge = Challenge.objects.get(type=ChallengeTypes.FINAL)
+
         other_games = GameTeam.objects.filter(game_side__game__status=SingleGameStatusTypes.DONE).filter(
             team=self.team).filter(
-            game_side__has_won=False)
+            game_side__has_won=False).filter(game_side__game__time__lt=challenge.end_time)
         if self.friendly_only:
             other_games = other_games.filter(game_side__game__match=None)
         if self.tournament_only:
             other_games = other_games.exclude(game_side__game__match=None)
         if self.final_challenge:
-            time = Challenge.objects.get(type=ChallengeTypes.FINAL).start_time
-            other_games = other_games.filter(game_side__game__time__gt=time)
+            other_games = other_games.filter(game_side__game__time__gt=challenge.start_time)
         other_games = other_games.values_list('game_side__game_id', flat=True)
 
         other_games = Game.objects.filter(id__in=other_games)
