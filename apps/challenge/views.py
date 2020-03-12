55c649518c4f4e1c3bd5f865dddac96b06ed6dd9
alireza <alirezashateri7@gmail.com>
2020-03-12 13:51:22 +0330

    Remove Final games from team dashboard game list

--- apps/challenge/views.py
+++ apps/challenge/views.py
@@ -82,10 +82,11 @@ class GamesListAPIView(GenericAPIView):
         except ValueError:
             offset = 0
             count = 50
-
-        game_ids = challenge_models.GameTeam.objects.filter(team=request.user.participant.team).values_list(
+        team = request.user.participant.team
+        time_limit = team.challenge.end_time
+        game_ids = challenge_models.GameTeam.objects.filter(team=team).values_list(
             'game_side__game_id', flat=True)
-        query = self.get_queryset().filter(id__in=game_ids).order_by('-time')
+        query = self.get_queryset().filter(id__in=game_ids).filter(time__lt=time_limit).order_by('-time')
         data = self.get_serializer(query[offset * count: offset * count + count], many=True).data
         return Response(data={'count': query.count(), 'games': data}, status=status.HTTP_200_OK)
 
